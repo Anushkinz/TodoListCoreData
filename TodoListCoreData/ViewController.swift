7db794f534dbaa2b67053349ed5ddd74f24d75e6
Anushkinz <anuarprogramm@gmail.com>
2021-07-04 17:04:30 +0600

    Initial Commit

---
+++ TodoListCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoListCoreData
+//
+//  Created by anushkinz on 4/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
